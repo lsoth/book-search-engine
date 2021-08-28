bd5f3e8706171333a12210888495d546975f6207
Mamba <84301026+lsoth@users.noreply.github.com>
2021-08-27 19:05:34 -0700

    updated client dependencies added queries and mutations to client, add boilerplate to server

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+const { gql } = require('@apollo/client');
+
+export const GET_ME = gql`{
+    
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        _id
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
